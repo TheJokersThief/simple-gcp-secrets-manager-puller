656b6e71b3873198116fc2611c868a153b6736a4
Evan Smith <esincork@gmail.com>
2020-07-05 22:38:55 +0200

    Add initial secret pulling code

---
+++ src/__init__.py
@@ -0,0 +1,35 @@
+from google.cloud import secretmanager
+
+
+def get_client(*args, **kwargs):
+    # Create the Secret Manager client.
+    return secretmanager.SecretManagerServiceClient(*args, **kwargs)
+
+
+def get_secret_versions(client, project_id, secret_id):
+    """
+    Get all enabled secret versions in the given secret and their metadata.
+    """
+
+    # Build the resource name of the parent secret.
+    parent = client.secret_path(project_id, secret_id)
+    
+    # Return all secret versions.
+    return [
+        version for version in client.list_secret_versions(parent)
+        if  version.state == secretmanager.enums.SecretVersion.State.ENABLED
+    ]
+
+
+def get_latest_value(client, project_id, secret_id, default):
+    versions = get_secret_versions(client, project_id, secret_id)
+    latest = versions[0].name
+
+    # Build the resource name of the secret version.
+    name = client.secret_version_path(project_id, secret_id, latest)
+
+    # Access the secret version.
+    response = client.access_secret_version(name)
+
+    payload = response.payload.data.decode('UTF-8')
+    return payload
