500d6fb7a4cb7fd1d7a5e94b43b9dcad2985a3d7
Evan Smith <esincork@gmail.com>
2020-09-29 14:12:13 +0200

    Ping secrets manager version

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     zip_safe=False,
     keywords='',
     classifiers=[],
-    install_requires=["google-cloud-secret-manager"],
+    install_requires=["google-cloud-secret-manager=1.0.0"],
 )
